4ffd59468d0e7fff1aa20952b84416d25f6a44b8
badiskefi <badiskefi@gmail.com>
2023-05-18 01:34:26 +0100

    theater spring boot frist commit

---
+++ src/main/java/com/us/theatre/entities/play/Play.java
@@ -0,0 +1,58 @@
+package com.us.theatre.entities.play;
+
+import java.util.Date;
+import java.util.List;
+
+import com.us.theatre.entities.actor.Actor;
+import com.us.theatre.entities.actor.ActorImage;
+import com.us.theatre.entities.category.Category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Play {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idPlay;
+	private String titlePlay;
+	private String subjectPlay;
+	private String biographyPlay;
+	private Long ticketsNumPlay;
+	private Date datePlay;
+	private Double pricePlay;
+	
+	@ManyToOne
+	private Category category;
+	
+	/*@OneToMany (mappedBy = "play")
+	 private List<PlayImage> images;
+	 private String imagePath;*/
+	
+	 @OneToOne
+	 private PlayImage image;
+	 private String imagePath;
+	
+	@ManyToMany
+	@JoinTable (
+			name = "plays_actors", joinColumns = @JoinColumn (name = "idPlay"),
+			inverseJoinColumns = @JoinColumn (name = "idActor")
+	)
+	private List<Actor> actors;
+}
